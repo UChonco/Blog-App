784f907c7b2c44e84e0f98d18ec99d4a966dfbab
Unathi <uchonco21@gmail.com>
2024-01-31 13:19:05 +0200

    Updating Client folder

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import Logo from '../images/logo.png'
+import { FaBarsStaggered } from "react-icons/fa6";
+import { AiOutlineClose } from "react-icons/ai";
 const Header = () => {
   return (
    <nav>
     <div className="container nav__container">
-     <Link to="/">
-        
+     <Link to="/" className="nav__logo">
+        <img src={Logo} alt="navbar-logo" />
      </Link>
+     <ul className="nav__menu">
+      <li><Link to="/profile">Ernest Achieve</Link></li>
+      <li><Link to="/create">Create Post</Link></li>
+      <li><Link to="/authors">Author</Link></li>
+      <li><Link to="/logout"></Link></li>
+     </ul>
+     <button className="nav__toggle-btn" >
+     <AiOutlineClose/>
+     </button>
     </div>
    </nav>
   )
